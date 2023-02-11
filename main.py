9134992f3511c85be39ec24e1a54c31de5a0b112
BotOleg <Nemerichlan@gmail.com>
2023-02-11 12:22:34 +0100

    as

--- main.py
+++ main.py
@@ -1,64 +1,62 @@
 import random
-class Student:
+class cat:
 
     def __init__(self, name):
         self.name = name
-        self.gladness = 50
-        self.progress = 0
+        self.rest = 50
+        self.eat = 0
         self.alive = True
 
-    def to_study(self):
-        print("Time to study")
-        self.progress += 0.12
-        self.gladness -= 3
+    def to_eat(self):
+        print("Time to eat")
+        self.eat += 0.12
+        self.rest -= 3
+
 
     def to_sleep(self):
         print("I will sleep")
-        self.gladness += 3
+        self.rest += 3
 
-    def to_chill(self):
+    def to_rest(self):
         print("Rest time")
-        self.gladness += 5
-        self.progress -= 0.1
+        self.rest += 5
+        self.eat -= 0.1
 
     def is_alive(self):
-        if self.progress < -0.5:
-            print("Cast out…")
+        if self.eat < -0.5:
+            print("die…")
             self.alive = False
-        elif self.gladness <= 0:
+        elif self.rest <= 0:
             print("Depression…")
             self.alive = False
-        elif self.progress > 5:
-            print("Passed externally…")
+        elif self.eat > 5:
+            print("I've eaten…")
             self.alive = False
 
     def end_of_day(self):
-        print(f"Gladness = {self.gladness}")
-        print(f"Progress = {round(self.progress, 2)}")
+        print(f"Gladness = {self.rest}")
+        print(f"Progress = {round(self.eat, 2)}")
 
     def live(self, day):
         day = "Day" + str(day) + "of" + self.name + "life"
         print(f"{day:=^50}")
         live_cube = random.randint(1, 3)
         if live_cube == 1:
-            self.to_study()
+            self.to_eat()
         elif live_cube == 2:
             self.to_sleep()
         elif live_cube == 3:
-            self.to_chill()
+            self.to_rest()
         self.end_of_day()
         self.is_alive()
 
-nick = Student(name="Nick")
-kate = Student(name="Kate")
+
+George = cat(name="George")
 for day in range(365):
-    if nick.alive == False:
+    if George.alive == False:
         break
-    nick.live(day)
-    if kate.alive == False:
-        break
-    kate.live(day)
-print(nick)
+    George.live(day)
+print(George)
 #
 #
 #
